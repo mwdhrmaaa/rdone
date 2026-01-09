139984000a074f07a90228ecbd54548ccc462bba
mwdhrmaaa <mwdhrmaaa@gmail.com>
2026-01-09 21:04:09 +0800

    Clean up duplicated model and prepare for routing

--- app/Models/Workout.php
+++ app/Models/Workout.php
@@ -3,22 +3,6 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-use App\Models\User; // Added this use statement for the Schedule model
-
-class Schedule extends Model
-{
-    protected $fillable = ['user_id', 'workout_id', 'day_of_week', 'scheduled_at'];
-
-    public function user()
-    {
-        return $this->belongsTo(User::class);
-    }
-
-    public function workout()
-    {
-        return $this->belongsTo(Workout::class);
-    }
-}
 
 class Workout extends Model
 {
